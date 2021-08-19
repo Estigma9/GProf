9b6c6c3eca469418e932461be59dcac7ac2cee42
Estigma9 <55663413+Estigma9@users.noreply.github.com>
2021-08-18 19:22:09 -0500

    Update index.js

--- frontend/src/components/Signin/index.js
+++ frontend/src/components/Signin/index.js
@@ -1,37 +1,3 @@
-/*
-
-import React from 'react'
-// import { Button } from '../ButtonElements'
-import {Container,FormWrap,Icon,Form,FormButton,FormatContent,Text,FormatLabel,FormatInput,FormH1,IconRe} from './SigninElements'
-
-
-const SignIn = () => {
-    return (
-        <>
-            <Container>
-                < FormWrap>
-                    <Icon to="/">GestoProf</Icon>
-                    <FormatContent>
-                        <Form action="#">
-                            <FormH1>Sign in to your account</FormH1>
-                            <FormatLabel htmlFor='for'>Email</FormatLabel>
-                            <FormatInput type='email' required></FormatInput>
-                            <FormatLabel htmlFor='for'>Password</FormatLabel>
-                            <FormatInput type='password' required></FormatInput>
-                            <FormButton type='submit'>Continue</FormButton>
-                            <Text>Forgot password</Text>
-                            <IconRe to="/register">Registrate aqui</IconRe>
-                        </Form>
-                    </FormatContent>
-                </FormWrap>
-            </Container>  
-        </>
-    )
-}
-
-export default SignIn
-
-*/
 
 import React, { Component } from 'react'
 // import { Button } from '../ButtonElements'
@@ -85,11 +51,11 @@ class SignIn extends Component {
             }
             */ 
         })  
-        /*.catch(error=>{
+        .catch(error=>{
             window.location.href="./student";
             console.log(error);
             swal("Oops!", "Algo salió mal!", "error");
-        })     */ 
+        })      
     }
 
     render() {  
